9eeb310a6717c8abb5512e669551d24ebe07ae27
Dhruv10143 <118088718+Dhruv10143@users.noreply.github.com>
2024-11-03 19:05:54 +0530

    Delete config-overrides.js

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-module.exports = function override(config) {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      console: require.resolve("console-browserify")
-    };
-    return config;
-  };
